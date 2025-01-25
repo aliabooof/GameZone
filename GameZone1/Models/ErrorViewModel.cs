d710ad6172e3ae1471a44e48dbc340a46f323a76
Ali Abooof <aliabooof@outlook.com>
2025-01-25 16:45:21 +0200

    Add project files.

---
+++ GameZone1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GameZone1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
